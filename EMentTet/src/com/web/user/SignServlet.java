ccaad7c172d4cea3511006624d4fa52547ff166c
bxl_glb <goodMorning_pro@at.bdqn.com>
2021-04-30 20:05:37 +0800

    ZXD完整

--- EMentTet/src/com/web/user/SignServlet.java
+++ EMentTet/src/com/web/user/SignServlet.java
@@ -14,6 +14,7 @@ import com.entity.Layui;
 import com.entity.Meun;
 import com.entity.User;
 import com.utils.PrintUtil;
+import com.utils.ReturnResult;
 import com.web.AbstractServlet;
 
 @SuppressWarnings("serial")
@@ -27,10 +28,10 @@ public class SignServlet extends AbstractServlet{
 	 * @return
 	 */
 	public String signBtn(HttpServletRequest request ,HttpServletResponse response) {
-		
-		 List<Meun> meunq =new MeunServiceImpl().QuanxianFind(10,1);
+		String lode=""+request.getSession().getAttribute("lodeid");
+		int lodeid=Integer.parseInt(lode);
+		 List<Meun> meunq =new MeunServiceImpl().QuanxianFind(10,lodeid);
 		  request.getSession().setAttribute("meunz", meunq);
-	   
 	      return "jsp/sign/signList";
 		}
 	
@@ -42,13 +43,40 @@ public class SignServlet extends AbstractServlet{
 	 * @throws IOException
 	 */
 	public void signAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user=""+request.getSession().getAttribute("userid");
+		int userid=Integer.parseInt(user);
+		String lode=""+request.getSession().getAttribute("lodeid");
+		int lodeid=Integer.parseInt(lode);
 		Layui<User> layui = new Layui<User>();
-		List<User> list = new SignServiceImpl().getUsers();
+		List<User> list = new SignServiceImpl().getUsers(userid,lodeid);
 		layui.setData(list);
 		layui.setCode(0);
 		layui.setMsg("");
 		layui.setCount(list.size());
 		PrintUtil.write(layui, response);
+	}
+	
+	/**
+	 * 我要签到
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	public ReturnResult isSgin(HttpServletRequest request ,HttpServletResponse response) {
+		ReturnResult result=new ReturnResult();
+		String user=""+request.getSession().getAttribute("userid");
+		int userid=Integer.parseInt(user);
+		int row=0;  //是否签到成功
+		int isSign=new SignServiceImpl().signCount(userid);  //获取你当天是否已签到
+		if (isSign==0) {  
+		row=new SignServiceImpl().addSign(userid);  //签到成功
+		}else {
+			row=2;
+		}
+		result.setData(row);
+		return result;
+		
+		
 	}
 	
 	@Override
